8e62464ad4bb47193b58f55f42441354cb2a2661
sirothiyapradhumn <60611214+sirothiyapradhumn@users.noreply.github.com>
2024-05-02 11:38:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import { productList } from '../store/productList';
+import Product from './components/Product';
+import './App.css';
+// import { store } from '../store';
+import { useSelector } from 'react-redux';
+
+export default function App() {
+  // bad way
+  // const productsByStore = store.getState().products;
+  const productsListByStore = useSelector((state) => state.products);
+  return (
+    <div className="products-container">
+      {productsListByStore.map(({ id, title, price, image, rating }) => (
+        <Product
+          key={id}
+          title={title}
+          rating={rating.rate}
+          price={price}
+          imageUrl={image}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ store/index.js
@@ -0,0 +1,31 @@
+import { combineReducers, createStore } from "redux";
+import wishlistReducer, { wishCartAddItem, wishCartRemoveItem } from "./wishlistReducer";
+import cartItemReducer, { cartAddItem, cartRemoveItem, decItemQuantity, incItemQuantity } from "./cartItemReducer";
+import productReducer from "./productReducer";
+
+
+const reducer = combineReducers({
+  products: productReducer,
+  cartItem: cartItemReducer,
+  wishList: wishlistReducer,
+});
+
+export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+
+store.dispatch(cartAddItem(1));
+store.dispatch(cartAddItem(6));
+store.dispatch(cartAddItem(11));
+store.dispatch(cartAddItem(20));
+store.dispatch(cartRemoveItem(6));
+
+store.dispatch(incItemQuantity(20));
+store.dispatch(incItemQuantity(20));
+
+store.dispatch(decItemQuantity(20));
+store.dispatch(decItemQuantity(20));
+store.dispatch(decItemQuantity(20));
+
+store.dispatch(wishCartAddItem(1));
+store.dispatch(wishCartAddItem(6));
+store.dispatch(wishCartAddItem(11));
+store.dispatch(wishCartRemoveItem(6));
